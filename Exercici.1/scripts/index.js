7c2065f4b6ba78846bec3db25c7de27a8f189b09
Anna Montserrat <amp1281@gmail.com>
2021-05-19 18:08:50 +0200

    ok

--- Exercici.1/scripts/index.js
+++ Exercici.1/scripts/index.js
@@ -1,10 +1,14 @@
+
 $(document).ready(function(){
     $("#button").click(function(e){
         e.preventDefault();
-        $.get("test.json",function(data){
-            console.log(data);
+        $.getJSON("http://api.icndb.com/jokes/random", function (data) {
+            $.each(data,function(index,item){
+                $("#texto").html(`
+                ${item.joke}
+            `);
+            });
         });
-
     });
-
 });
+
